19133c86d524b3fb5dcbca09889bd4bb830a9345
unknown <manishz90@yahoo.co.in>
2017-08-13 18:43:03 +0530

    Add project page

--- server.js
+++ server.js
@@ -48,4 +48,12 @@ app.get('/about', (req, res) =>{
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: "Projects"
+  });
+});
+
+
+
 app.listen(port);
